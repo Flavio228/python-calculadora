4794f9ac35a3bab487fe1718ffde67810f46543d
Flavio228 <246509266+Flavio228@users.noreply.github.com>
2026-02-26 23:05:56 +0100

    mejorada validacion de operador y control de división por cero usando match

--- calculadora.py
+++ calculadora.py
@@ -7,13 +7,18 @@ num2 = int(input("Ingrese un segundo número: "))
 match operacion:
   case "+":
     resultado = num1 + num2
+    print(f"El resultado de tu operación es: {resultado}")
   case "-":
     resultado = num1 - num2  
+    print(f"El resultado de tu operación es: {resultado}")
   case "*":
     resultado = num1 * num2
+    print(f"El resultado de tu operación es: {resultado}")
   case "/":
-    resultado = num1 / num2
+    if num2 == 0:
+      print("Error: No se puede dividie entre 0.")
+    else:
+      resultado = num1 / num2
   case _:
     print("Operador ingresado incorrecto")  
 
-print(f"El resultado de tu operación es: {resultado}")
